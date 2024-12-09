b416b845c1009a59188bbc25da5329b111f95259
Ehenew <ehenew16@gmail.com>
2024-12-09 21:58:21 +0300

    Small bug fix

--- src/js/controller.js
+++ src/js/controller.js
@@ -149,6 +149,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+// Simulating git branching
+const newFeature = function () {
+  console.log('Welcome to Forkify Application');
+};
 // Publisher-Subscriber Pattern
 const init = function () {
   bookmarksView.addHandlerRenderBookmark(controlBookmarks);
@@ -158,6 +162,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature()
 };
 init();
 
